d0aa132db59a62ee1853c420f517dba921e0b149
CharsiBabu <im.charsi@gmail.com>
2013-07-13 12:46:36 +0530

    test

---
+++ common/include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 70 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#70 PREEMPT Sat Apr 20 07:42:52 IST 2013"
+#define LINUX_COMPILE_TIME "07:42:52"
+#define LINUX_COMPILE_BY "deviant-kernel"
+#define LINUX_COMPILE_HOST "noOb-lab"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2009q3-68) "
